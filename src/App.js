ce5a4a109875054af86d3a76d575cd92f12419db
Tyler Casperson <tylercasperson@gmail.com>
2020-07-26 23:07:39 -0600

    setup basic search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+import SearchBar from './components/layout/SearchBar';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -10,21 +12,9 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <SearchBar />
+    </Fragment>
   );
 };
 
