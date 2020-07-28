776e2505464073756721010828d9b6999bcf547f
Tyler Casperson <tylercasperson@gmail.com>
2020-07-27 23:11:43 -0600

    setup a basic reducer

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,20 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
+
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+};
