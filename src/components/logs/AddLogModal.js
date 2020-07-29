19edb12072489878528117bda274cd821a5a79fe
Tyler Casperson <tylercasperson@gmail.com>
2020-07-28 20:05:58 -0600

    tech list is being populated from the database

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import TechSelectOptions from '../techs/TechSelectOptions';
 
 const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
@@ -60,9 +61,7 @@ const AddLogModal = ({ addLog }) => {
               <option value='' diabled='true'>
                 Select Technician
               </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
